6fa9b4c1ff27dfbd30d68399d73e639dc2a3184b
viacheslavrudenko <slavikrudenko@gmail.com>
2023-01-13 11:30:02 +0200

    add comment

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -8,6 +8,7 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 });
 
+//if we have and need to use token for authorization
 export const setAuthToken = (token: Token) => {
     if (token) {
         instance.defaults.headers.common["Authorization"] = token;
