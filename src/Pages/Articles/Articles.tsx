0e0539a7cd074ec7afa5852189c7da85f685eece
viacheslavrudenko <slavikrudenko@gmail.com>
2023-01-13 12:41:57 +0200

    added logic on Articles page

--- src/Pages/Articles/Articles.tsx
+++ src/Pages/Articles/Articles.tsx
@@ -1,5 +1,32 @@
+import { Alert, Box } from "@mui/material";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import PageError from "../../Components/PageInfo/PageError";
+import PageLoading from "../../Components/PageInfo/PageLoading";
+import { RootState } from "../../store/root-reducer";
+import { useActions } from "../../utils/hooks/useActions";
+
 const ArticlesPage = () => {
-  return <></>;
+  const { axiosData } = useActions();
+  useEffect(() => {
+    axiosData();
+  }, []);
+
+  const { data, err, loading } = useSelector(
+    (state: RootState) => state.Articles
+  );
+
+  return (
+    <Box>
+      {err ? (
+        <PageError content={err} />
+      ) : loading ? (
+        <PageLoading />
+      ) : (
+        <Box>Content</Box>
+      )}
+    </Box>
+  );
 };
 
 export default ArticlesPage;
