0ffae5d3067d831fbaba0535e165c07e880e22d7
viacheslavrudenko <slavikrudenko@gmail.com>
2023-01-16 01:24:34 +0200

    added search input action

--- src/Components/Articles/Filter/FilterItem.tsx
+++ src/Components/Articles/Filter/FilterItem.tsx
@@ -2,13 +2,19 @@ import React, { FunctionComponent, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
 import { Box, FormControl, InputAdornment, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/root-reducer";
+import { Article } from "../../../types/article";
+import { filteredArticles } from "./functions";
 
 const Search: FunctionComponent = () => {
   const [showClearIcon, setShowClearIcon] = useState("none");
   const [value, setValue] = useState<string>("");
+  const articles = useSelector((state: RootState) => state.Articles.data);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setShowClearIcon(event.target.value === "" ? "none" : "flex");
+    console.log(filteredArticles(articles, value));
   };
 
   const handleClick = (): void => {
