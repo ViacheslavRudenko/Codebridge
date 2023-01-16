883130a3ca3b7440f510bc902001e746082d7a24
viacheslavrudenko <slavikrudenko@gmail.com>
2023-01-17 00:40:34 +0200

    added default mui styles

--- src/utils/styles/default.ts
+++ src/utils/styles/default.ts
@@ -3,7 +3,35 @@ import { createTheme } from "@mui/material";
 const style = createTheme({
     typography: {
         fontFamily: ["Montserrat", "sans-serif"].join(","),
-    }
+        fontSize: 16,
+        fontWeightMedium: 400,
+        subtitle1: {
+            fontWeight: 600,
+            color: 'rgba(54, 54, 54, 1)',
+        },
+        subtitle2: {
+            color: 'rgba(54, 54, 54, 0.8)',
+            lineHeight: 1
+        },
+        h6: {
+            color: 'rgba(54, 54, 54, 0.8)',
+            fontSize: 24,
+            lineHeight: 1
+        },
+        body1: {
+            color: 'rgba(54, 54, 54, 0.7)',
+            fontSize: 14
+        }
+    },
+
+    palette: {
+        primary: {
+            main: 'rgba(54, 54, 54, 0.7)',
+        },
+        secondary: {
+            main: 'rgba(54, 54, 54, 1)',
+        },
+    },
 });
 
 export default style 
