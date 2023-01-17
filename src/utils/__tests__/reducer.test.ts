f8ae2bbe4583d23380f2300e9b8f7eadfd6bd28a
viacheslavrudenko <slavikrudenko@gmail.com>
2023-01-18 00:00:52 +0200

    upd test after store changing

--- src/utils/__tests__/reducer.test.ts
+++ src/utils/__tests__/reducer.test.ts
@@ -1,4 +1,4 @@
-import { Article } from "../../types/article";
+import { Article, FilteredArticles } from "../../types/article";
 import { ArticlesActionTypes } from "../../types/store";
 import reducer from "../../store/Articles/reducer";
 
@@ -11,6 +11,7 @@ describe("Articles reducer", () => {
           loading: true,
           err: "",
           data: [],
+          filteredArticles: []
         },
         {
           type: ArticlesActionTypes.FETCH_ARTICLES,
@@ -20,29 +21,31 @@ describe("Articles reducer", () => {
       loading: true,
       err: "",
       data: [],
+      filteredArticles: []
     });
   });
 
-  it("should to set articles data", () => {
+  it("should to set articles data as array", () => {
     expect(
       reducer(
         {
           loading: false,
           err: "",
-          data: data,
+          data: articleArr,
+          filteredArticles: []
         },
         {
           type: ArticlesActionTypes.FETCH_ARTICLES_SUCCESS,
-          payload: data,
+          payload: articleArr,
         }
       )
     ).toEqual({
       loading: false,
       err: "",
-      data: data,
+      data: articleArr,
+      filteredArticles: []
     });
   });
-
   it("should to set error and reset list", () => {
     expect(
       reducer(
@@ -50,6 +53,7 @@ describe("Articles reducer", () => {
           loading: false,
           err: "Server error",
           data: [],
+          filteredArticles: []
         },
         {
           type: ArticlesActionTypes.FETCH_ARTICLES_ERROR,
@@ -60,22 +64,51 @@ describe("Articles reducer", () => {
       loading: false,
       err: "Server error",
       data: [],
+      filteredArticles: []
+    });
+  });
+  it("should to set filterrd article list", () => {
+    expect(
+      reducer(
+        {
+          loading: false,
+          err: "",
+          data: [],
+          filteredArticles: filteredArticles,
+        },
+        {
+          type: ArticlesActionTypes.FILTERED_ARTICLES,
+          payload: filteredArticles,
+        }
+      )
+    ).toEqual({
+      loading: false,
+      err: "",
+      data: [],
+      filteredArticles: filteredArticles,
     });
   });
 });
 
-const data: Article[] = [
-  {
-    "id": 17872,
-    "title": "NASA to Announce Major Eco-Friendly Aviation Project Update",
-    "url": "http://www.nasa.gov/press-release/nasa-to-announce-major-eco-friendly-aviation-project-update",
-    "imageUrl": "https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb_0.jpg?itok=mrBnB_c9",
-    "newsSite": "NASA",
-    "summary": "Media are invited to a news conference with NASA Administrator Bill Nelson and other agency leadership at 10 a.m. EST on Wednesday, Jan. 18, at NASA Headquarters in Washington.",
-    "publishedAt": "2023-01-12T22:12:00.000Z",
-    "updatedAt": "2023-01-12T22:12:27.404Z",
-    "featured": false,
-    "launches": [],
-    "events": []
-  }
-];
+const data: Article =
+{
+  "id": 17872,
+  "title": "NASA to Announce Major Eco-Friendly Aviation Project Update",
+  "url": "http://www.nasa.gov/press-release/nasa-to-announce-major-eco-friendly-aviation-project-update",
+  "imageUrl": "https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb_0.jpg?itok=mrBnB_c9",
+  "newsSite": "NASA",
+  "summary": "Media are invited to a news conference with NASA Administrator Bill Nelson and other agency leadership at 10 a.m. EST on Wednesday, Jan. 18, at NASA Headquarters in Washington.",
+  "publishedAt": "2023-01-12T22:12:00.000Z",
+  "updatedAt": "2023-01-12T22:12:27.404Z",
+  "featured": false,
+  "launches": [],
+  "events": []
+}
+
+const articleArr = [data]
+
+const filteredArticles: FilteredArticles[] = [{
+  item: data,
+  refIndex: 1,
+  matches: [{ indices: [[3, 2]], value: 'string', key: 'string' }]
+}]
