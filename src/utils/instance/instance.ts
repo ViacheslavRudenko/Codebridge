023ea97c657030520126271a18057c45b03f79a7
viacheslavrudenko <slavikrudenko@gmail.com>
2023-01-17 23:49:36 +0200

    deleted unuseful code

--- src/utils/instance/instance.ts
+++ src/utils/instance/instance.ts
@@ -1,20 +1,11 @@
 import axios from "axios";
 import { Token } from "../../types/api";
 
-const token: Token = process.env.REACT_APP_API_TOKEN || "";
 const url: Token = process.env.REACT_APP_API_URL || "";
 
 const instance = axios.create({
     baseURL: url
 });
 
-//if we have and need to use token for authorization
-export const setAuthToken = (token: Token) => {
-    if (token) {
-        instance.defaults.headers.common["Authorization"] = token;
-    } else {
-        delete instance.defaults.headers.common["Authorization"];
-    }
-};
 
 export default instance;
